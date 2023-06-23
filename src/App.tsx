a87377fa79258a52dd0a17cff58b0b0ab798f054
Alena Gerasimova <alena.gerasimova1999@gmail.com>
2023-06-23 23:17:56 +0400

    themes and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React, {Suspense} from 'react';
-import "./index.scss";
 import {Link, Route, Routes} from "react-router-dom";
 import {MainPageAsync} from "./pages/main/MainPage.async";
 import {AboutPageAsync} from "./pages/about/AboutPage.async";
+import {useTheme} from "./theme/useTheme";
+import "./styles/index.scss";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Suspense fallback={<div>Loading...</div>}>
