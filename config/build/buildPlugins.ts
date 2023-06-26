0aebd3d2fc68b84499c7d9b08c4650d21eb7fbf9
Alena Gerasimova <alena.gerasimova1999@gmail.com>
2023-06-26 18:17:41 +0400

    upd

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -15,6 +15,7 @@ export default function buildPlugins({paths, isDev}: BuildOptions): webpack.Webp
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
-        })
+        }),
+        new webpack.HotModuleReplacementPlugin()
     ];
 }
